a096a3794427a24d5bedf7cd41ff9ec0dae58567
NastyonaSavina <nastyona.savina@gmail.com>
2023-03-10 21:13:59 +0200

    finish

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Node-js-hw1
+
+всі контакти
+http://prntscr.com/zrYHo_kzP7h3
+
+контакт по id
+http://prntscr.com/crs4vv1uQkuU
+
+Додаємо контакт
+http://prntscr.com/w0wA1n_3W36b
+
+Видаляємо контакт
+http://prntscr.com/WOjIXkt4o-Jx
---
+++ contacts.js
@@ -0,0 +1,92 @@
+
+const fs = require('fs').promises;
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+
+
+const contactsPath = path.join(__dirname, '/db/contacts.json');
+
+
+const listContacts = async () => {
+
+    try {
+        const listResult = await fs.readFile(contactsPath, "utf-8");
+        return JSON.parse(listResult); 
+    }
+    catch (error) {
+        console.log(`Something went wrong.. ${error.message}`);
+
+    }
+    
+}
+
+
+const getContactById = async (contactId) => {
+
+    try {
+        const contacts = await listContacts();
+        const contactById = contacts.find((contact) => contact.id === contactId);
+        if (!contactId) {
+            return null;
+        }
+    return contactById;
+    }
+     catch (error) {
+        console.log(`Something went wrong.. ${error.message}`);
+
+    }
+   
+}
+
+const removeContact = async (contactId) => {
+
+    try {
+        const contacts = await listContacts();
+     
+        const updatedListContacts = contacts.filter(contact => contact.id !== contactId);
+        await fs.writeFile(contactsPath, JSON.stringify(updatedListContacts));
+        return updatedListContacts;
+    }
+    catch (error) {
+        console.log(`Something went wrong.. ${error.message}`);
+
+    }
+
+}
+
+
+
+
+
+
+const addContact = async (name, email, phone) => {
+    
+    try {
+        const newContact = {
+            id: uuidv4(),
+            name,
+            email,
+            phone,
+        };
+        const contacts = await listContacts();
+        contacts.push(newContact);
+
+        await fs.writeFile(contactsPath, JSON.stringify(contacts));
+
+
+        return newContact;
+    }
+    catch (error) {
+        console.log(`Something went wrong.. ${error.message}`);
+    }
+  
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+  
+};
